b54d6ac074b605ab1ad2466badbb4f39cff227b1
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-09 18:55:00 +0530

    Create 16 Finding the Maximum Scalar Product of two vectors.java

---
+++ Arrays/16 Finding the Maximum Scalar Product of two vectors.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Demo{
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,6,3,7};
+        int[] arr2 = {10,7,45,3,7};
+
+        int n = arr1.length;
+
+        Arrays.sort(arr1);
+
+        Arrays.sort(arr2);
+
+        int product = 0;
+        for(int i = 0; i<n; i++){
+            product += arr1[i]*arr2[i];
+       }
+       System.out.print(product+" ");
+    }
+}
+
+O/P:
+413
