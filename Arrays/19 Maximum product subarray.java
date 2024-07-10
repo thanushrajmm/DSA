d7b8dc902bef017b474b2560200661e11073579f
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-11 00:13:09 +0530

    Create 19 Maximum product subarray.java

---
+++ Arrays/19 Maximum product subarray.java
@@ -0,0 +1,25 @@
+Practice link: [https://bit.ly/3VPdyyq]
+
+Example:
+Sample Input 1:
+4
+1 -2 3 -4
+Sample Output 1:
+24
+
+Solution: 
+public class Solution {
+    public static int subarrayWithMaxProduct(int []arr){
+        int pre = 1; int suf =1;     //initialize pre and suff as 1;
+        int ans = Integer.MIN_VALUE; 
+        int n = arr.length;
+        for(int i =0; i<n;i++){
+            if(pre==0) pre = 1;     //if pre and suff becomes 0 then reset the value to 1; 
+            if(suf ==0) suf =1;
+            pre = pre*arr[i];       //pre multiplies from starting of the array
+            suf = suf*arr[n-i-1];   //suff multiplies from end of the array
+            ans = Math.max(ans,Math.max(pre,suf)); //compare the pre and suff max, then compare it with ans.
+         }
+            return ans;    //return the max value
+    }
+}
