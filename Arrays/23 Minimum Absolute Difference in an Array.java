12e1b30b345676cdd16b72dc04c83428af1fd41c
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-11 17:39:09 +0530

    Update 23 Minimum Absolute Difference in an Array.java

--- Arrays/23 Minimum Absolute Difference in an Array.java
+++ Arrays/23 Minimum Absolute Difference in an Array.java
@@ -1,14 +1,26 @@
-    public static int minimumAbsoluteDifference(List<Integer> arr) {
+1. Minimum Absolute difference:
+public static int minimumAbsoluteDifference(List<Integer> arr) {
     int n = arr.size();
     Collections.sort(arr);
-        int minAbsDifference = Integer.MAX_VALUE; // to track the minimum absolute difference
+        int minAbsDifference = Integer.MAX_VALUE; 
         
         for (int i = 1; i < n; i++) {
-            int diff = Math.abs(arr.get(i) - arr.get(i-1));   
-            if (diff < minAbsDifference) {
-                minAbsDifference = diff;
+            int diff = Math.abs(arr.get(i) - arr.get(i-1));   //using Math.abs to find the minimum value ( (i)-(i-1) ) 
+            if (diff < minAbsDifference) {                    //if the diff is less than minAbsDiff 
+                minAbsDifference = diff;                      //put diff in minAbsDiff
             }
         }
-        return minAbsDifference;
+        return minAbsDifference;                              //return minAbsDiff
     }
+}
+
+---------------------------------------------------------------------------------------------------
+2. Sum of Minimum Absolute Difference:
+    public static int sumOfMinAbsDifferences(List<Integer> arr) {
+    Collections.sort(arr);
+    int sum = 0;                                    //initially the sum is 0
+    for (int i = 1; i < arr.size(); i++) {          
+        sum += Math.abs(arr.get(i) - arr.get(i-1)); //adding the value of ( (i)-(i-1) ) to sum
+    }
+    return sum;                                      //then returning the sum.
 }
