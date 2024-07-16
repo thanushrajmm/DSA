71182507688064034c9b4c717052d590ca46f9c3
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-17 03:15:22 +0530

    Update 01 Print N times Using Recursion.java

--- Recursion/01 Print N times Using Recursion.java
+++ Recursion/01 Print N times Using Recursion.java
@@ -1,1 +1,17 @@
+Input:
+5
+Output:
+GFG GFG GFG GFG GFG;
 
+class Solution {
+    int count = 1;
+    public void print(int N){
+        if(count > N) return;
+        System.out.print("GFG ");
+        count++;
+        print(N);
+    }
+    void printGfg(int N) {
+        print(N);
+    }
+}
