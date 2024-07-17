c97c5edf6ab696b2cb8ddba5925885ddde0313fd
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-17 12:32:44 +0530

    Update 01 Print N times Using Recursion.java

--- Recursion/01 Print N times Using Recursion.java
+++ Recursion/01 Print N times Using Recursion.java
@@ -2,9 +2,9 @@ Input:
 5
 Output:
 GFG GFG GFG GFG GFG;
-
+//With Global Variable
 class Solution {
-    int count = 1;
+    int count = 1; //global variable;
     public void print(int N){
         if(count > N) return;
         System.out.print("GFG ");
@@ -13,5 +13,19 @@ class Solution {
     }
     void printGfg(int N) {
         print(N);
+    }
+}
+---------------------------------------------------------------------
+//without Global variable
+class Solution {
+
+    public void print(int i,int N){
+        if(i>N) return;
+        System.out.print("GFG ");
+        print(i+1,N);
+    }
+    void printGfg(int N) {
+        // code here
+        print(1,N);       //including parameter;
     }
 }
