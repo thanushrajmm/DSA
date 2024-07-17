d51351d2da2f1243aa7076eb2c07caf348d5bf08
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-17 13:04:30 +0530

    Update 02 Print N to 1.java

--- Recursion/02 Print N to 1.java
+++ Recursion/02 Print N to 1.java
@@ -12,3 +12,16 @@ class Solution {
         print(N);
     }
 }
+----------------------------------------------------------------
+//using Backtracking
+class Solution {
+public void print(int i, int N){
+    if(i>N) return;
+    print(i+1,N);
+    System.out.print(i+" ");
+}
+    void printNos(int N) {
+        print(1,N);
+        
+    }
+}
