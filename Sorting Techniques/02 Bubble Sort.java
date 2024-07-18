ec58099b38feb94edd4cab2756e68cb4a1a10453
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-18 17:34:15 +0530

    Create 02 Bubble Sort.java

---
+++ Sorting Techniques/02 Bubble Sort.java
@@ -0,0 +1,22 @@
+Problem Link -> [https://www.geeksforgeeks.org/problems/bubble-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bubble-sort]
+class Solution
+{
+    //Function to sort the array using bubble sort algorithm.
+	public static void bubbleSort(int arr[], int n)
+    {
+        //code here
+        for(int i = n-1; i>= 1; i--){
+            int didSwap = 0;
+            for(int j = 0; j<= i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    didSwap = 1;
+                }
+            }
+        if(didSwap == 0)
+        break;
+        }
+    }
+}
