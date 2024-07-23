72054d452ebf319fb7a2de6dfa8bc44c0a97c3f5
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-24 02:08:36 +0530

    Update 01 Count Nodes of LL.java

--- LinkedList/01 Count Nodes of LL.java
+++ LinkedList/01 Count Nodes of LL.java
@@ -3,29 +3,6 @@ Sample Input 1 :
 3 4 5 2 6 1 9 -1
 Sample Output 1 :
 7
-class Node {
-     public int data;
-     public Node next;
-    
-     Node()
-     {
-         this.data = 0;
-         this.next = null;
-     }
-    
-     Node(int data)
-     {
-         this.data = data;
-         this.next = null;
-     }
-    
-     Node(int data, Node next)
-     {
-         this.data = data;
-         this.next = next;
-     }
- }
-
 public class Solution {
     public static int length(Node head){
         //Your code goes here
