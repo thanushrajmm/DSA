913a61ab62f501b0099f94a1ada8c48844fed143
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-24 01:50:48 +0530

    Create 03 Search in a LL.java

---
+++ LinkedList/03 Search in a LL.java
@@ -0,0 +1,15 @@
+Problem Link -> [https://tinyurl.com/27c9b349]
+
+public class Solution
+{
+    public static int searchInLinkedList(Node head, int k)
+    {
+        // Write Your Code Here.
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == k) return 1;
+            temp = temp.next;
+        }
+        return 0;
+    }
+}
