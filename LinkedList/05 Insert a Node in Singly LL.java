18730b9bea4aed07f3c99f09de01370e7feb9cee
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-25 11:10:19 +0530

    Create 05 Insert a Node in Singly LL.java

---
+++ LinkedList/05 Insert a Node in Singly LL.java
@@ -0,0 +1,17 @@
+class Demo{
+    private static Node insertBefVa(Node head, int el, int val){
+        if(head=null) return null;
+        if(head.data== val){
+            return new Node(el,head);
+        }
+        Node temp = head;
+        while(temp.next != null){
+            if(temp.next.data == val){
+                Node x = new Node(el,temp.next);
+                temp.next = x;
+                break;
+            }
+            temp = temp.next;
+        }    
+        return head;
+    }
