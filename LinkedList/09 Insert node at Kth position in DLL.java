716e16c2e7718e1f8abdc31e31b78bab98d885d6
Thanushraj M M <thanushrajmm@gmail.com>
2024-07-25 17:44:37 +0530

    Create 09 Insert node at Kth position in DLL.java

---
+++ LinkedList/09 Insert node at Kth position in DLL.java
@@ -0,0 +1,21 @@
+problem link -> [https://www.geeksforgeeks.org/problems/insert-a-node-in-doubly-linked-list/1]
+class GfG
+{
+    //Function to insert a new node at given position in doubly linked list.
+    void addNode(Node head_ref, int pos, int data)
+	{
+		// Your code here
+		Node temp = head_ref;
+		Node newNode = new Node(data);
+		int count = 1;
+		while(count <=pos){
+		    temp = temp.next;
+		    count++;
+		}
+		Node ptr = temp.next;
+		
+		newNode.prev = temp;
+		temp.next = newNode;
+		newNode.next = ptr;
+	}
+}
