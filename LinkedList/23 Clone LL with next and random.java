d1de406fb0f04aabf6e9007c20e664b6d73cb245
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-07 12:38:42 +0530

    Update 23 Clone LL with next and random.java

--- LinkedList/23 Clone LL with next and random.java
+++ LinkedList/23 Clone LL with next and random.java
@@ -23,3 +23,54 @@ class Solution {
         return map.get(head);
     }
 }  
+---------------------------------------------------------------------------------------------
+    2. Optimal approach:
+class Solution {
+
+    void printInBetween(Node head){
+        Node temp = head;
+        while(temp != null){
+            Node nextElement = temp.next;
+            Node copyNode = new Node(temp.val);
+            copyNode.next = nextElement;
+            temp.next = copyNode;
+            temp = nextElement;
+        }
+    }
+
+    void connectRandomPointers(Node head){
+        Node temp = head;
+        while(temp != null){
+
+            Node copyNode = temp.next;
+
+            if(temp.random != null){
+                copyNode.random = temp.random.next;
+            }
+            else {
+                copyNode.random = null;
+            }
+
+            temp = temp.next.next;
+        }
+    }
+    
+    public static Node connectNextPointers(Node head){
+        Node temp = head;
+        Node dummyNode = new Node(-1);
+        Node res = dummyNode;
+        while(temp != null){
+            res.next = temp.next;
+            res = res.next;
+
+            temp.next = temp.next.next;
+            temp = temp.next;
+        }
+        return dummyNode.next;
+    }
+    public Node copyRandomList(Node head) {
+        printInBetween(head);
+        connectRandomPointers(head);
+        return connectNextPointers(head);
+    }
+}
