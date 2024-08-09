6aeca85bcdfc933846ced6d8726419bbb4294ddb
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-09 14:55:10 +0530

    Create 03 Stack using LinkedList.java

---
+++ Stack and Queue/03 Stack using LinkedList.java
@@ -0,0 +1,33 @@
+class MyStack {
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }
+    StackNode top;
+
+    // Function to push an integer into the stack.
+    void push(int a) {
+        // Add your code here
+        StackNode temp = new StackNode(a);
+        temp.next = top;
+        top = temp;
+    }
+
+    // Function to remove an item from top of the stack.
+    int pop() {
+        // Add your code here
+        if(top == null){
+            return -1;
+        }
+        else{
+            StackNode temp = top;
+            top = top.next;
+            int value = temp.data;
+            return value;
+        }
+    }
+}
