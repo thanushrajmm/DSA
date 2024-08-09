1a57c1ecf62230b726ffe8514eb3d3c3385cd1bf
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-10 01:01:49 +0530

    Update 01 2sum.java

--- Arrays II /01 2sum.java
+++ Arrays II /01 2sum.java
@@ -14,3 +14,23 @@ class Solution {
         return new int[]{-1, -1};
     }
 }
+---------------------------------------------------------------------------------------------------------------------
+    2. better approach using Hashing 
+    class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        return new int[]{}; // No solution found
+    }
+}
+------------------------------------------------------------------------------------------------------------------
+   3. if the questionns ask only to print YES or NO then use two pointers approach for optimised solution
