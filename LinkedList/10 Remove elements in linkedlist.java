5a90d68bfe97987ff3f695ed2eda6fec2e51ea48
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-19 18:35:30 +0530

    Update 10 Remove elements in linkedlist.java

--- LinkedList/10 Remove elements in linkedlist.java
+++ LinkedList/10 Remove elements in linkedlist.java
@@ -1,4 +1,16 @@
 problem link -> [https://leetcode.com/problems/remove-linked-list-elements/description/]
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode temp1 = null;
