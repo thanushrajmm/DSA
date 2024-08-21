e9e9b961d5edbf8e4cc2c56476234f40661b56eb
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-21 11:46:52 +0530

    Create 03 Detect Capital.java

---
+++ Strings/03 Detect Capital.java
@@ -0,0 +1,12 @@
+problem link -> [ https://leetcode.com/problems/detect-capital/ ]
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count = 0;
+        for(char c : word.toCharArray()){
+            if(Character.isUpperCase(c)){
+                count++;
+            }
+        }
+        return count == 0 || count==word.length() || (count==1 && Character.isUpperCase(word.charAt(0)));
+    }
+}
