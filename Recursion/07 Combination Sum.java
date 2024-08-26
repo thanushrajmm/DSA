81a6f351127ae2b870f321efc09b2c7bee3416bd
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-26 21:34:40 +0530

    Update 07 Combination Sum.java

--- Recursion/07 Combination Sum.java
+++ Recursion/07 Combination Sum.java
@@ -1,6 +1,10 @@
 problem link -> [ https://leetcode.com/problems/combination-sum/ ]
 Time complexity: O(2^t)
 Space complexity: O(t+k)
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
