e8e660e482aaff400064f047ed6a5b362383c2fa
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-26 21:34:59 +0530

    Update 08 Combination Sum II.java

--- Recursion/08 Combination Sum II.java
+++ Recursion/08 Combination Sum II.java
@@ -1,4 +1,15 @@
 problem link -> [ https://leetcode.com/problems/combination-sum-ii/ ] 
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8
+Output: 
+[
+[1,1,6],
+[1,2,5],
+[1,7],
+[2,6]
+]
+    
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         List < List < Integer >> ans = new ArrayList < > ();
