81af20a562380df11142216962dec956edb72ab7
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-26 22:45:06 +0530

    Update Subset Sum I.java

--- Recursion/Subset Sum I.java
+++ Recursion/Subset Sum I.java
@@ -1,4 +1,12 @@
 problem link -> [ https://www.geeksforgeeks.org/problems/subset-sums2234/1 ]
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
 class Solution {
     ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int n) {
         // code here
