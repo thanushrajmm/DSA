fa00b951362c271816b39d458d28bb102464763f
Thanushraj M M <thanushrajmm@gmail.com>
2024-08-29 08:38:45 +0530

    Create 02 Binary Tree Representation.java

---
+++ Trees/02 Binary Tree Representation.java
@@ -0,0 +1,13 @@
+link -> [ https://www.geeksforgeeks.org/problems/binary-tree-representation/1 ] 
+class Solution{
+    public static void createTree(Node root0, ArrayList<Integer> v ){
+        // Code here
+        root0.val = v.get(0);
+        root0.left = new Node(v.get(1));
+        root0.right = new Node(v.get(2));
+        root0.left.left = new Node(v.get(3));
+        root0.left.right = new Node(v.get(4));
+        root0.right.left = new Node(v.get(5));
+        root0.right.right = new Node(v.get(6));
+    }
+}
