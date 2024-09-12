278ecc459d7f892025ab6313e204b2869c1d8f5c
Thanushraj M M <thanushrajmm@gmail.com>
2024-09-12 21:19:31 +0530

    Create 06 Number of occurrence.java

---
+++ Binary Search/06 Number of occurrence.java
@@ -0,0 +1,54 @@
+problem link -> [ https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1 ]
+class Solution {
+    int count(int[] arr, int n, int x) {
+        // code here
+        int lowbound = lowboundindex(arr,x);
+        int upperbound = upperboundindex(arr,x);
+        int count = upperbound - lowbound + 1;
+        
+        if(lowbound == -1 && upperbound ==-1){
+            return 0;
+        }
+        return count;
+    }
+    
+    int lowboundindex(int[] arr, int x){
+        int s = 0;
+        int e = arr.length-1;
+        int index =-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid] == x){
+                index = mid;
+                e = mid-1;
+            }
+            else if(arr[mid]<x){
+                s = mid +1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        return index;
+    }
+    
+    int upperboundindex(int[] arr, int x){
+        int s = 0;
+        int e = arr.length-1;
+        int index =-1;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid] == x){
+                index = mid;
+                s = mid +1;
+            }
+            else if(arr[mid]<x){
+                s = mid +1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+        return index;
+    }
+}
