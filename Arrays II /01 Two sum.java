5fd37a5d60f815c19907eb49aa4a66842787e1a8
Thanushraj M M <thanushrajmm@gmail.com>
2024-09-20 09:13:59 +0530

    Update 01 Two sum.java

--- Arrays II /01 Two sum.java
+++ Arrays II /01 Two sum.java
@@ -1,13 +1,13 @@
 Problem link -> [https://leetcode.com/problems/two-sum/description/]
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=1;i<nums.length;i++)
+        for(int i=0;i<nums.length;i++)
         {
-            for(int j=i;j<nums.length;j++)
+            for(int j=i+1;j<nums.length;j++)
             {
-                if (nums[j] + nums[j - i] == target)
+                if (nums[i] + nums[j] == target)
                 {
-                    return new int[]{j, j-i};
+                    return new int[]{i, j};
                 }
             }
         }
