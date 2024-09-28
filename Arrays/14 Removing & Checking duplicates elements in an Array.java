dfcdb11525a639c697a8e5a5d6d2ed8824751d4d
Thanushraj M M <thanushrajmm@gmail.com>
2024-09-28 16:27:10 +0530

    Update 14 Removing & Checking duplicates elements in an Array.java

--- Arrays/14 Removing & Checking duplicates elements in an Array.java
+++ Arrays/14 Removing & Checking duplicates elements in an Array.java
@@ -39,3 +39,25 @@ O/P:
             return false;
     }
 }
+---------------------------------------------------------------------------------
+public class Main {
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+	
+	int n = sc.nextInt();
+
+	int[] arr = new int[n];
+	for(int i = 0; i<n; i++){
+		arr[i] = sc.nextInt();
+	}
+	HashSet<Integer> set = new HashSet<>();
+
+	for(int i=0; i<n; i++){
+		set.add(arr[i]);
+	}
+
+	Integer[] result = set.toArray(new Integer[0]);
+	System.out.print(Arrays.toString(result));
+	
+	}
+}
