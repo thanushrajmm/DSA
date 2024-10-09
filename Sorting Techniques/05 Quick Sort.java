dbc4709d89701f26bc6e147fbe6d30bccbd875e6
Thanushraj M M <thanushrajmm@gmail.com>
2024-10-09 22:23:55 +0530

    Update 05 Quick Sort.java

--- Sorting Techniques/05 Quick Sort.java
+++ Sorting Techniques/05 Quick Sort.java
@@ -1,38 +1,49 @@
 //[4, 2, 7, 3, 8, 5, 1]
-class Solution
-{
-    //Function to sort an array using quick sort algorithm.
-    static void quickSort(int arr[], int low, int high)
-    {
-        // code here
-        if(low<high){                            
-            int pIndex =partition(arr,low,high);  
-            quickSort(arr,low,pIndex-1);
-            quickSort(arr,pIndex+1,high);
-        }
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static int partition(int arr[], int low, int high){
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+        while(i<j){
+           while(arr[i]<=pivot && i<high){
+               i++;
+           }
+           while(arr[j]>=pivot && j>low){
+               j--;
+           }
+           if(i<j){
+               int temp = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+           }
     }
-    static int partition(int arr[], int low, int high)
-    {
-        int pivot = arr[low];                        // Select a pivot;
-        int i = low;                                 //set i pointer in low;
-        int j = high;                                //set j pointer in high;
-        
-        while(i<j){                                  //till i<j loop runs
-            while(arr[i]<=pivot && i<=high-1){       //if arr[i]<=pivot and i<=high-1;
-                i++;                                 //increment the i pointer;
-            } 
-            while(arr[j]>pivot && j>=low+1){         //if arr[j]>pivot and j>=low+1;
-                j--;                                 //decrement j pointer;
-            }
-            if(i<j){                                 //swap arr[i] and arr[j] 
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+    int temp1 = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp1;
+    
+    return j;
+    }
+    public static void quicksort(int arr[], int low, int high){
+        if(low<high){
+        int pIndex = partition(arr,low,high);
+        quicksort(arr,low,pIndex-1);
+        quicksort(arr,pIndex+1,high);
+    }
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		quicksort(arr, 0, n-1);
+		for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
         }
-        int temp1 = arr[low];                        //after the i becomes greater than j;
-        arr[low] = arr[j];                          //swap arr[low](pivot) and arr[j];
-        arr[j] = temp1;
-        return j;
-    } 
-}
+		
+	    }
+    }
