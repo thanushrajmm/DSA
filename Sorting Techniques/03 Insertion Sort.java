dd237794705a0eb45f99109915b01fb563e6478e
Thanushraj M M <thanushrajmm@gmail.com>
2024-10-09 20:24:59 +0530

    Update 03 Insertion Sort.java

--- Sorting Techniques/03 Insertion Sort.java
+++ Sorting Techniques/03 Insertion Sort.java
@@ -1,24 +1,28 @@
 Problem link -> [https://www.geeksforgeeks.org/problems/insertion-sort/0]
-class Solution
-{
-  static void insert(int arr[],int i)
-  {
-       int n = arr.length;
-       for(int k=0; k<=n-1; k++){
-           int j=k;
-           while( j>0 && arr[j-1]>arr[j]){
-               int temp = arr[j-1];
-               arr[j-1] = arr[j];
-               arr[j] = temp;
-               j--;
-           }
-       }
-       for(int m =0; i<n; i++){
-           System.out.print(arr[m]);
-       }
-  }
-  public void insertionSort(int arr[], int n)
-  {
-      insert(arr,n);
-  }
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		
+		for(int i=0; i<=n-1; i++){
+		    int j = i;
+		    while(j>0 && arr[j-1]>arr[j]){
+		        int temp = arr[j-1];
+		        arr[j-1] = arr[j];
+		        arr[j] = temp;
+		        j--;
+		    }
+		}
+		
+		for(int i = 0; i<n; i++){
+		    System.out.print(arr[i]+" ");
+	}
+}
 }
