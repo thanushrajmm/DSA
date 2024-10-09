ddafbcb534cae77b22e2923cee5ae2769c36072c
Thanushraj M M <thanushrajmm@gmail.com>
2024-10-09 20:00:26 +0530

    Update 02 Bubble Sort.java

--- Sorting Techniques/02 Bubble Sort.java
+++ Sorting Techniques/02 Bubble Sort.java
@@ -1,24 +1,32 @@
 Problem Link -> [https://www.geeksforgeeks.org/problems/bubble-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=bubble-sort]
-class Solution
-{
-    //Function to sort the array using bubble sort algorithm.
-	public static void bubbleSort(int arr[], int n)
-    {
-        //code here
-        for(int i = n; i>0; i--){
-            int didSwap = 0;
-            for(int j = 0; j<i-1; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    didSwap = 1;
-                }
-            }
-        if(didSwap == 0)
-        break;
-        }
-    }
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		for(int i=0; i<n-1; i++){
+			int didSwap = 0;
+			for(int j=0; j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+				int temp = arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+				didSwap=1;
+		}
+		}
+		if(didSwap==0){
+			break;
+		}
+	}
+	for(int i = 0; i<n; i++){
+	System.out.print(arr[i]+" ");
+}
+}
 }
 TC -> O(n^2);
 SC -> O(1);
