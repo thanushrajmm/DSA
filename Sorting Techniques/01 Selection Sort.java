5bc17cd4bf63d7757dbe7d9452d29aa7d3ad5cdc
Thanushraj M M <thanushrajmm@gmail.com>
2024-10-09 19:29:45 +0530

    Update 01 Selection Sort.java

--- Sorting Techniques/01 Selection Sort.java
+++ Sorting Techniques/01 Selection Sort.java
@@ -1,27 +1,31 @@
 Problem Link -> [https://www.geeksforgeeks.org/problems/selection-sort/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=selection-sort]
 
-class Solution
-{
-	int  select(int arr[], int i)
-	{
-            int mini = i;
-            for(int j = i+1; j<arr.length; j++ ){
-                if(arr[j]<arr[mini]){
-                    mini = j;
-                }
-            }
-        return mini;
-	}
-	
-	void selectionSort(int arr[], int n)
-	{
-	    //code here
-	    for(int i = 0; i<n-1; i++){
-	        int mini = select(arr,i);
-	        int temp = arr[mini];
-	        arr[mini] = arr[i];
-	        arr[i] = temp;
-	    }
+import java.io.*;
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i<n; i++){
+			arr[i] = sc.nextInt();
+		}
+		
+		
+		for(int i = 0; i<n-1; i++){
+			int mini = i;
+			for(int j = i+1; j<n; j++ ){
+				if(arr[j]<arr[mini]){
+					mini = j;
+				}
+			}
+			int temp = arr[mini];
+			arr[mini]=arr[i];
+			arr[i] = temp;
+		}
+		for(int i = 0; i<n; i++){
+			System.out.print(arr[i]+" ");
+		}
 	}
 }
 TC -> O(n^2);
