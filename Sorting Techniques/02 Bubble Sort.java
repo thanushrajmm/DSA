bbfca466f60c6e10916d031c75240ed31dc86797
Thanushraj M M <thanushrajmm@gmail.com>
2024-10-09 19:34:37 +0530

    Update 02 Bubble Sort.java

--- Sorting Techniques/02 Bubble Sort.java
+++ Sorting Techniques/02 Bubble Sort.java
@@ -7,7 +7,7 @@ class Solution
         //code here
         for(int i = n; i>0; i--){
             int didSwap = 0;
-            for(int j = 0; j<=i; j++){
+            for(int j = 0; j<i-1; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
