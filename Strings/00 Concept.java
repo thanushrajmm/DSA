42938e91eee9f8f32161f4aaafb2d2685c79ddd8
Thanushraj M M <thanushrajmm@gmail.com>
2024-11-19 20:52:39 +0530

    Update 00 Concept.java

--- Strings/00 Concept.java
+++ Strings/00 Concept.java
@@ -1,8 +1,16 @@
 when the input in given in the String form - "100 200 300".
   to convert this into an integer;
 import java.util.*;
-public class main{
+public class Main{
   public static void main(Strings[] arg){
-  String input = sc.nextLine();
-
+    
+  Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    sc.nextLine(); 
+    String input = sc.nextLine();
+    String[] parts = input.split(" ");
+    int[] values = new int[n];
+    
+   for (int i = 0; i < n; i++) {
+            values[i] = Integer.parseInt(parts[i].trim());
   }
