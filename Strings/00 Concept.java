baa9359e064c518f0986b458564f5524276e7057
Thanushraj M M <thanushrajmm@gmail.com>
2024-11-19 20:48:15 +0530

    Create 00 Concept.java

---
+++ Strings/00 Concept.java
@@ -0,0 +1,8 @@
+when the input in given in the String form - "100 200 300".
+  to convert this into an integer;
+import java.util.*;
+public class main{
+  public static void main(Strings[] arg){
+  String input = sc.nextLine();
+
+  }
