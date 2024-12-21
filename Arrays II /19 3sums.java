d224bceb74034a08007ae253739359b5234da8fd
Thanushraj M M <thanushrajmm@gmail.com>
2024-12-21 09:53:20 +0530

    Update 19 3sums.java

--- Arrays II /19 3sums.java
+++ Arrays II /19 3sums.java
@@ -21,3 +21,36 @@ class Solution {
         return ans;
     }
 }
+-------------------------------------------------------------------------------------------
+Optimal 2 pointers approach -> O(N^2);
+    class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for(int i = 0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                List<Integer> sortedArray = Arrays.asList(nums[i],nums[j],nums[k]);
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }
+                else if(sum > 0){
+                    k--;
+                }
+                else{
+                    ans.add(sortedArray);
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+}
