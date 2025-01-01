a51c794fabed09839ac8f50d1635767d6940278c
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-01 21:10:00 +0530

    Update 21 Merge two sorted LL.java

--- LinkedList/21 Merge two sorted LL.java
+++ LinkedList/21 Merge two sorted LL.java
@@ -1,30 +1,29 @@
-problem link -> [ https://bit.ly/3vjuaGQ ]
-public class Solution {
-	public static LinkedListNode<Integer> sortTwoLists(LinkedListNode<Integer> first, LinkedListNode<Integer> second) {
-		// Write your code here.
-        LinkedListNode<Integer> t1 = first;
-        LinkedListNode<Integer> t2 = second;
-        LinkedListNode<Integer> dummyNode = new LinkedListNode<Integer>(-1);
-        LinkedListNode<Integer> temp = dummyNode;
 
-        while (t1 != null && t2 != null){
-            if(t1.data < t2.data){
-                temp.next = t1;
-                temp = t1;
-                t1 = t1.next; 
-            }
-            else{
-                temp.next = t2;
-                temp = t2;
-                t2 = t2.next;
-            }
+class Solution {
+    public static ListNode convertArrToLL(ArrayList<Integer> arr){
+        ListNode dummyNode = new ListNode(-1);
+        ListNode temp = dummyNode;
+        int n = arr.size();
+        for(int i = 0; i<n; i++){
+            temp.next = new ListNode(arr.get(i));
+            temp = temp.next;
         }
-
-        if(t1!=null) temp.next = t1;
-        else temp.next = t2;
         return dummyNode.next;
-        
-	}
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        while(list1 != null){
+            arr.add(list1.val);
+            list1 = list1.next;
+        }
+        while(list2 != null){
+            arr.add(list2.val);
+            list2 = list2.next;
+        }
+        Collections.sort(arr);
+        return convertArrToLL(arr);
+    }
 }
 -----------------------------------------------------------------------------------------------------------------------------------
 problem link -> [ https://leetcode.com/problems/merge-two-sorted-lists/ ]
