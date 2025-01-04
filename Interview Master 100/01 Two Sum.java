8c1a652b532d02a551d099a5ef8373ba7da226c6
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-04 11:58:04 +0530

    Create 01 Two Sum.java

---
+++ Interview Master 100/01 Two Sum.java
@@ -0,0 +1,35 @@
+Problem Statement: Given an array of integers arr[] and an integer target.
+
+1st variant: Return YES if there exist two numbers such that their sum is equal to the target. Otherwise, return NO.
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. Otherwise, we will return {-1, -1}.
+
+* Brute Force -> TC - O(N^2) , SC - O(1);
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
+------------------------------------------------
+*  Better/Optimised Approach -> TC - O(N), SC - O(N); If(Variant 2)-Optimised.
+
+import java.util.HashMap;
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{i, map.get(complement)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}                                                                                         
