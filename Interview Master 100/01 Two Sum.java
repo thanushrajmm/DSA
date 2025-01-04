fff6855b26861dacdd9c9b8a854f8adaa457463b
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-04 12:09:27 +0530

    Update 01 Two Sum.java

--- Interview Master 100/01 Two Sum.java
+++ Interview Master 100/01 Two Sum.java
@@ -32,4 +32,28 @@ class Solution {
         }
         return new int[]{-1, -1};
     }
-}                                                                                         
+}
+---------------------------------------------
+if array is sorted - optimised Approach ->
+    class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        int left = 0;
+        int right = n-1;
+
+        while(left<right){
+            int sum = numbers[left]+numbers[right];
+            if(sum == target){
+                return new int[] {left+1,right+1};
+            }
+            else if(sum<target){
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+    return new int[] {};
+    }
+}
+                                                                                                                      
