bf7b1916f136f3951597ec4c5e65e0a46790a84b
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-05 14:38:38 +0530

    Update 14 Missing Number.java

--- Interview Master 100/14 Missing Number.java
+++ Interview Master 100/14 Missing Number.java
@@ -14,3 +14,15 @@ class Solution {
         return n;
     }
 }
+--------------------------------------------------------------------------------
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int sum1 = n*(n+1)/2;
+        int sum2 = 0;
+        for(int i = 0; i<n; i++){
+            sum2 = sum2 +nums[i];
+        }
+        return sum1-sum2;
+    }
+}
