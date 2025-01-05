859f98a994b778f9c75168a3ea093d1bed58bbfd
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-05 14:33:40 +0530

    Create 14 Missing Number.java

---
+++ Interview Master 100/14 Missing Number.java
@@ -0,0 +1,16 @@
+TC -> O(NlogN)
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for(int i = 0; i<n; i++){
+            if(i==nums[i]){
+                continue;
+            }
+            else{
+                return i;
+            }
+        }
+        return n;
+    }
+}
