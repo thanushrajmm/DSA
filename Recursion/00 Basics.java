4ab735d63232a23a0c5beb0f2c55153bdca42298
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-05 18:25:12 +0530

    Update 00 Basics.java

--- Recursion/00 Basics.java
+++ Recursion/00 Basics.java
@@ -1,3 +1,4 @@
+1. Find GCD
 class Main {
     public static int gcd(int a, int b){
     if(b==0){
@@ -7,5 +8,18 @@ class Main {
 }
     public static void main(String[] args) {
         System.out.print(gcd(48,18));
+    }
+}
+2. Find Tower of hanoi
+    class Main {
+    public static void toh(int n, char A, char B, char C){
+        if(n>0){
+            toh(n-1,A,C,B);
+            System.out.println("from "+A+" to "+C);
+            toh(n-1,B,A,C);
+        }
+    }
+    public static void main(String[] args) {
+        toh(3,'A','B','C');
     }
 }
