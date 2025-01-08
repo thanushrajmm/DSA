abc9ae1465eb570d958ae18acbbfa062ece53689
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-08 10:47:09 +0530

    Update 51 Search in rotated Sorted array.java

--- Interview Master 100/51 Search in rotated Sorted array.java
+++ Interview Master 100/51 Search in rotated Sorted array.java
@@ -1,3 +1,4 @@
+1st Varient ->
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
@@ -26,5 +27,43 @@ class Solution {
         }
     }
         return -1;
+    }
+}
+---------------------------------------------------------------------------------------------------------------
+2nd varient ->
+    class Solution {
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target) return true;
+
+            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low = low+1;
+                high = high-1;
+                continue;
+            }
+
+            if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && target <= nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        else{
+            if(nums[mid]<=target && target<=nums[high]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+    }
+        return false; 
     }
 }
