cd893c3ca33207d5007e9718b4d3f50f59dbecf1
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-15 21:33:04 +0530

    Create 25 Remove Duplicates from sorted list.java

---
+++ LinkedList/25 Remove Duplicates from sorted list.java
@@ -0,0 +1,14 @@
+link - > [ https://leetcode.com/problems/remove-duplicates-from-sorted-list/ ]
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+
+        while(temp != null && temp.next != null){
+            if(temp.val == temp.next.val){
+                temp.next = temp.next.next;
+            }
+            else temp = temp.next;
+        }
+        return head;
+    }
+}
