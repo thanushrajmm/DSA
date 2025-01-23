13fe289244c271550a8f62ff940f7b9c45192739
Thanushraj M M <thanushrajmm@gmail.com>
2025-01-23 19:10:39 +0530

    Create 21 Remove Duplicates from sorted array II.java

---
+++ Arrays II /21 Remove Duplicates from sorted array II.java
@@ -0,0 +1,14 @@
+link -> [ https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/ ] 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int k = 2;
+
+        for(int i = 2; i<nums.length; i++){
+            if(nums[i] != nums[k-2]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
