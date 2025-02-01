05a84c61c497e9f799e4cf5dcc78bf51cbf3a298
Thanushraj M M <thanushrajmm@gmail.com>
2025-02-01 22:34:00 +0530

    Update 22 Product of Array except Self.java

--- Interview Master 100/22 Product of Array except Self.java
+++ Interview Master 100/22 Product of Array except Self.java
@@ -16,3 +16,22 @@ class Solution {
         return answer;
     }
 }
+-------------------------------
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result,1);
+        int pre = 1, pos = 1;
+
+        for(int i = 0; i<nums.length; i++){
+            result[i] = pre;
+            pre = nums[i]*pre;
+        }
+
+        for(int i = nums.length-1; i>=0; i--){
+            result[i] = result[i]*pos;
+            pos = nums[i]*pos;
+        }
+        return result;
+    }
+}
