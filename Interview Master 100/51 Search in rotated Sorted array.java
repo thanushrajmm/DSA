1596d5bc1ca100e7373f40935f7ad26ede8f8b4b
Thanushraj M M <thanushrajmm@gmail.com>
2025-02-02 21:51:24 +0530

    Update 51 Search in rotated Sorted array.java

--- Interview Master 100/51 Search in rotated Sorted array.java
+++ Interview Master 100/51 Search in rotated Sorted array.java
@@ -1,69 +1,31 @@
-1st Varient ->
 class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
-        int low = 0;
-        int high = n-1;
+        int left = 0;
+        int right = n-1;
 
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target) return mid;
-
-            if(nums[low]<=nums[mid]){
-            if(nums[low]<=target && target <= nums[mid]){
-                high = mid-1;
+        while(left<=right){
+            int mid = (left + right) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] <= nums[mid]){
+                if(nums[left] > target || target > nums[mid]){
+                    left = mid +1;
+                }
+                else{
+                    right = mid - 1;
+                }
             }
             else{
-                low = mid+1;
+                if(target < nums[mid] || target > nums[right] ){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid+1;
+                }
             }
         }
-        else{
-            if(nums[mid]<=target && target<=nums[high]){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-    }
         return -1;
-    }
-}
----------------------------------------------------------------------------------------------------------------
-2nd varient ->
-    class Solution {
-    public boolean search(int[] nums, int target) {
-        int n = nums.length;
-        int low = 0;
-        int high = n-1;
-
-        while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target) return true;
-
-            if(nums[low] == nums[mid] && nums[mid] == nums[high]){
-                low = low+1;
-                high = high-1;
-                continue;
-            }
-
-            if(nums[low]<=nums[mid]){
-            if(nums[low]<=target && target <= nums[mid]){
-                high = mid-1;
-            }
-            else{
-                low = mid+1;
-            }
-        }
-        else{
-            if(nums[mid]<=target && target<=nums[high]){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
-            }
-        }
-    }
-        return false; 
     }
 }
