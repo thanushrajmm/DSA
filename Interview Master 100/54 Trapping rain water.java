f86fc2749e213047ffef74f62dfa9411b4bbe1e2
Thanushraj M M <thanushrajmm@gmail.com>
2025-02-05 00:26:32 +0530

    Create 54 Trapping rain water.java

---
+++ Interview Master 100/54 Trapping rain water.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int trap(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = height[0];
+        int rightMax = height[right];
+        int total = 0;
+        while(left<right){
+            if(height[left] < height[right]){
+                leftMax = Math.max(leftMax, height[left]);
+                if(leftMax - height[left] > 0){
+                    total += leftMax - height[left];
+                }
+                left++;
+            }
+            else{
+                rightMax = Math.max(rightMax,height[right]);
+                if(rightMax - height[right] > 0){
+                    total += rightMax - height[right];
+                }
+                right--;
+            }
+        }
+        return total;
+    }
+}
