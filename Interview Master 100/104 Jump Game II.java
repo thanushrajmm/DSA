aea1cfd80f06575c15322bf15eeeaf6430311a22
Thanushraj M M <thanushrajmm@gmail.com>
2025-02-06 21:46:01 +0530

    Update 104 Jump Game II.java

--- Interview Master 100/104 Jump Game II.java
+++ Interview Master 100/104 Jump Game II.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jump = 0;
+        int currMax = 0;
+        int currEnd = 0;
 
+        for(int i = 0; i<nums.length-1; i++){
+            currMax = Math.max(currMax , i + nums[i]);
+            if(i == currEnd){
+                jump++;
+                currEnd = currMax;
+            }
+        }
+        return jump;   
+    }
+}
